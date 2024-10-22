92e69fdf06adf5c3665dd0982e166ca00784c93b
yujianni123 <153381821+yujianni123@users.noreply.github.com>
2024-10-22 09:35:01 +0800

    Add files via upload

---
+++ easyX/easyX/图形库.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <graphics.h>
+#include <conio.h>
+int main()
+{
+	//创建一个窗口
+
+	initgraph(100, 400);
+	outtextxy(200,100,L"hahaha");
+	return 0;
+}
